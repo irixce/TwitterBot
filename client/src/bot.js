7f601e5c5a5c65820e44e9e76b227c5ad814d3b8
ira <ira.ceka001@umb.edu>
2020-04-28 02:20:35 -0400

    GET twitter followers test

--- client/src/bot.js
+++ client/src/bot.js
@@ -1,10 +1,13 @@
-//import Twitter from 'twitter-lite';
 var Twitter = require("twitter-lite");
-var auth_keys = require("./auth");
+var auth_keys = require("./auth"); //contains Twitter API authentication keys
 
 const client = new Twitter(auth_keys, {
   subdomain: "api", // "api" is the default (change for other subdomains)
   version: "1.1", // version "1.1" is the default (change for other subdomains)
    });
 
-client.get("followers/ids", {screen_name: "" }).then(results => { console.log("results", results);}).catch(console.error);
+client.get("followers/ids",
+    {screen_name: "" })
+    .then(results => {
+        console.log("results", results);
+    }).catch(console.error);

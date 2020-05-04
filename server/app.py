ddc9438caebf5d6c9aa900d2a4d7848b08df7d41
ira <ira.ceka001@umb.edu>
2020-05-04 04:58:24 -0400

    testing on adding to database from Twitter API

--- server/app.py
+++ server/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from subprocess import call
 
 print("here")
 DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 screen_names = ["samp1"]
+db_list = []
+db_dict = {}
 
 @app.route('/usernamesLink', methods=['GET', 'POST'])
 def usernames():
@@ -25,6 +29,25 @@ def usernames():
         return jsonify({
         'status': 'success',
         'screenNames': screen_names
+    })
+
+@app.route('/database', methods=['GET', 'POST'])
+def db():
+    if request.method == 'POST':
+        res = list(request.get_json())
+        db_list.append(list(request.get_json()))
+        print("POSTTTTTTTTT")
+        print(res[0]['screen_name'])
+        db_dict[res[0]['screen_name'].lower()] = {'protected': res[0]['protected'], 'description': res[0]['description']}
+        print(db_dict)
+        #print(list(request.get_json()))
+    else:
+        call(["node", "bot.js"])
+        #print(db_list[0][0]["protected"])
+        return jsonify({
+        'status': 'success',
+        'db': db_dict
+
     })
 
 if __name__ == '__main__':
---
+++ server/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_secret: "MuqN0HigNytfegLgklOgz9TMGTPVXq5L5kHgcUcPwGzLacE5MF" // from Twitter.
+     ,consumer_key: "9s7ATsdLGgKQizUgLw6jV7rhO" // from Twitter.
+     ,access_token_key: "1240871753343283200-XQPpwYWRFLaTpY7isD2r7T1OCpZYdC" // from your User (oauth_token)
+     ,access_token_secret: "6oWuBgfqJ0ezoLVvqTi0Ue6jE0yf8J8ZwFqQCj72uu9TK" // from your User (oauth_token_secret)
+ };
